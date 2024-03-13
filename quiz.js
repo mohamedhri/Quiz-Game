333626413a2b8bb96f2f719513127ea613567588
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-03-13 11:51:19 +0000

    add the data Array of our qsm in js

---
+++ quiz.js
@@ -0,0 +1,44 @@
+
+const quizData = [
+ {
+  question: "Quelle affirmation est vraie à propos de Java?",
+  a: "Java est un langage de programmation dépendant de la séquence",
+  b: "Java est un langage de programmation dépendant du code",
+  c: "Java est un langage de programmation dépendant de la plate-forme",
+  d: "Java est un langage de programmation indépendant de la plate-forme",
+  correct: "d"
+ },
+
+ {
+  question: "Quel composant est utilisé pour compiler, déboguer et exécuter les programmes Java?",
+  a: "JRE",
+  b: "JIT",
+  c: "JDK",
+  d: "Machine virtuelle Jva",
+  correct: "c"
+ },
+ {
+  question: "Laquelle des fonctionnalités suivantes n'est pas une fonctionnalité Java?",
+  a: "Orienté objet",
+  b: "Utilisation de pointeurs",
+  c: "Portable",
+  d: "Dynamique et Extensible",
+  correct: "b"
+ },
+ {
+  question: "Lequel de ces éléments ne peut pas être utilisé pour un nom de variable en Java?",
+  a: "identifiant et mot-clé",
+  b: "identifiant",
+  c: "mot-clé",
+  d: "aucun des éléments mentionnés",
+  correct: "c"
+ },
+ {
+  question: "Quelle est l'extension des fichiers de code Java?",
+  a: ".js",
+  b: ".txt",
+  c: ".classe",
+  d: ".java",
+  correct: "d"
+ }
+];
